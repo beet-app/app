3aa1c947abe6be83b8eaa5e46353fec9ddcec594
thiagokroger <thiagokroger@gmail.com>
2015-02-24 17:55:48 -0300

    go

---
+++ gulp/inject.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var paths = gulp.paths;
+
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep').stream;
+
+gulp.task('inject', function () {
+
+  var injectStyles = gulp.src([
+    paths.src + '/{app,components,assets}/**/*.css'
+  ], { read: false });
+
+  var injectScripts = gulp.src([
+    paths.src + '/{app,components}/*.js',
+    paths.src + '/{app,components}/**/*.js',
+    '!' + paths.src + '/{app,components}/**/*.spec.js',
+    '!' + paths.src + '/{app,components}/**/*.mock.js'
+  ]);
+
+  var injectOptions = {
+    ignorePath: [paths.src, paths.tmp + '/serve'],
+    addRootSlash: false
+  };
+
+  var wiredepOptions = {
+    directory: 'bower_components'
+  };
+
+  return gulp.src(paths.src + '/**/*.html')
+    .pipe($.inject(injectStyles, injectOptions))
+    .pipe($.inject(injectScripts, injectOptions))
+    .pipe(wiredep(wiredepOptions))
+    .pipe(gulp.dest(paths.tmp + '/serve'));
+
+});
