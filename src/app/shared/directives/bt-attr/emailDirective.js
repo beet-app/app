1232a8b4522732a5655ac842f9b103b4d042793c
gpferreira <gustavo.ferreira08@gmail.com>
2015-03-26 11:20:59 -0300

    going

---
+++ src/app/shared/directives/bt-attr/emailDirective.js
@@ -0,0 +1,22 @@
+﻿BeetApp
+  .directive("btEmail", function (Common, $compile) {
+
+
+    return {
+      //replace: true,
+      restrict: 'E',
+      compile: function(element, attributes){
+
+        return {
+          pre: function(scope, element, attributes, controller, transcludeFn){
+            scope.type = "email";
+          },
+          post: function(scope, element, attributes, controller, transcludeFn){
+
+          }
+        }
+      },
+      templateUrl: Common.getDirectiveTemplateUrl("bt-attr/text")
+    };
+
+  });
